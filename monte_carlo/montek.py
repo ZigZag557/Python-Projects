1807bc24870afee00eb51c553c3d4b69584c98cf
Berke <bek_2001@hotmail.com>
2020-01-11 14:01:48 +0300

    Commented both monte_carlo and taylor_apprx codes.

--- monte_carlo/montek.py
+++ monte_carlo/montek.py
@@ -1,10 +1,17 @@
 
-import math
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptc
 
+
+
+# Function to be integrated.
 def func(x):
 	return ((x**2)-1)/((x**2)+1)
+#-----------------------------
+
+
+
+# Get N amount of (x,y) points on the function from the starting point to the end point.
 
 def getPoints(N,start,end):
 	xPoints = []
@@ -20,10 +27,20 @@ def getPoints(N,start,end):
 
 	return [xPoints,yPoints]
 
+#------------------------------------------------------------
+
+# Make axes below the graphs.
 plt.rc('axes', axisbelow=True)
+
+# Enable multi graph drawing.
 fig, ax = plt.subplots()
+
+# Enable grids.
 ax.grid(True, which='both')
 
+
+# Draw N amount of rectangles with same width under the graph from the start point to the end point.
+# Return sum of the rectangles' area.
 def monteCarloApprox(N, start, end):
 
 	total = 0
@@ -41,14 +58,19 @@ def monteCarloApprox(N, start, end):
 		curXpos += incrementAmt
 		total += incrementAmt * func(curXpos)
 	return total
+#-------------------------------------------------------------------------------------------------
 
-points = getPoints(500,-15,15)
-
+# Draw x and y axes.
 ax.axhline(y=0, color='k', zorder = -1)
 ax.axvline(x=0, color='k', zorder = -1)
+
+#Draw the graph.
+points = getPoints(500,-15,15)
 ax.plot(points[0],points[1], color= 'red')
 
+# Draw the rectangles and writing approximated integral value.
 approxVal = str(monteCarloApprox(500,-10,10))
 ax.set_xlabel("Integral value is approximately: " + approxVal)
+
 plt.show()
 
--- taylor_apprx/taylor.py
+++ taylor_apprx/taylor.py
@@ -6,9 +6,11 @@ import matplotlib.patches as ptc
 
 x = Symbol('x')
 
+#Primary function that taylor series will be based on.
 primaryFunction = sin(x)
 
 
+# Get N amount of (x,y) points of the function from the starting point to the end point.
 def getFuncPoints(N,start,end, function):
 	xPoints = []
 	yPoints = []
@@ -22,8 +24,9 @@ def getFuncPoints(N,start,end, function):
 		yPoints.append( function.subs(x, curXpos) )
 
 	return [xPoints,yPoints]
+#----------------------------------------------------------------------------------------
 
-
+# Get up to N'th step of Taylor series.
 def getTaylorFunc(N, a):
 
 	funcStr = str(primaryFunction.subs(x, a))
@@ -35,23 +38,31 @@ def getTaylorFunc(N, a):
 
 	print(simplify(sympify(funcStr)))
 	return simplify(sympify(funcStr))
+#-----------------------------------------
 
-
-
+# Make axes below the graphs.
 plt.rc('axes', axisbelow=True)
 
+# Enable multi graph drawing.
 fig, ax = plt.subplots()
+
+# Enable grids.
 ax.grid(True, which='both')
+
+# Draw x and y axes.
 ax.axhline(y=0, color='k', zorder = -1)
 ax.axvline(x=0, color='k', zorder = -1)
 
+#Draw the primary function graph.
 points = getFuncPoints(500,-20, 20, primaryFunction)
 ax.plot(points[0],points[1], color= 'red')
 
+#Draw the taylor series function.
 taylorFunc = getTaylorFunc(11, 0)
 taylorPoints = getFuncPoints(500,-5,5, taylorFunc)
 ax.plot(taylorPoints[0], taylorPoints[1], color= 'blue')
 
+# Write the taylor series if it's short enough.
 if len(str(taylorFunc)) < 80:
 	ax.set_xlabel("y = " + str(taylorFunc))
 
