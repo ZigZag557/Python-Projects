808cc2af6959ae3afb98f8f5e598d1cc3a6c5ad9
Berke <bek_2001@hotmail.com>
2020-01-16 16:13:42 +0300

    Added second order differential equation support to approximator.

--- diff_apprx/diff.py
+++ diff_apprx/diff.py
@@ -7,8 +7,6 @@ import matplotlib.patches as ptc
 
 x = Symbol('x')
 f = Function('f')(x)
- 
-diff_eq = Derivative(f, x) + f/2 + x
 
 
 # Make axes below the graphs.
@@ -25,13 +23,12 @@ ax.axhline(y=0, color='k', zorder = -1)
 ax.axvline(x=0, color='k', zorder = -1)
 
 
-def firstOrderTaylorEq(fA, A, N):
+def firstOrderTaylorEq(fA, A, N, function):
 
-	newDiffEq = diff_eq
 	taylorEq = fA
 
 	#First derivative is problematic in the for loop so we do it here.
-	firstEq = diff_eq.subs(f,fA)
+	firstEq = function.subs(f,fA)
 	firstEq = solve(firstEq, Derivative(fA, x, 1))[0]
 
 	lastDerivative = [Derivative(f, x), firstEq]
@@ -39,13 +36,41 @@ def firstOrderTaylorEq(fA, A, N):
 
 	for k in range(N - 1):
 
-		eq = diff(diff_eq, x, k + 1 )
+		eq = diff(function, x, k + 1 )
 		eq = eq.replace(lastDerivative[0], lastDerivative[1])
 		eq = solve(eq, Derivative(f, (x, k + 2)))[0]
 		eqDerivativeVal = eq.subs(x, A)
 		lastDerivative = [ Derivative(f, x, k + 2), eqDerivativeVal ]
 
 		taylorEq += (eqDerivativeVal * (x-A)**(k+1))/factorial(k+1)
+
+	print(taylorEq)
+	drawFunction(500, -20, 20, taylorEq)
+
+
+def secondOrderTaylorEq(fA, f1A, A, N, function):
+
+	taylorEq = fA
+
+
+	firstEq = function.subs(f, fA)
+	firstEq = solve(firstEq, Derivative(fA, x, 2))[0]
+
+	higherFuncVal = firstEq
+	lesserFuncVal = f1A
+
+	for k in range(N-1):
+
+		eq = diff(function, x, k + 1 )
+		eq = eq.replace(Derivative(f, x, k + 2), higherFuncVal)
+		eq = eq.replace(Derivative(f, x, k + 1), lesserFuncVal)
+		eq = solve(eq, Derivative(f, x, k + 3))[0]
+		eqDerivativeVal = eq.subs(x, A)
+
+		lesserFuncVal = higherFuncVal
+		higherFuncVal = eqDerivativeVal
+		taylorEq += (eqDerivativeVal * (x-A)**(k))/factorial(k)
+
 	print(taylorEq)
 	drawFunction(500, -20, 20, taylorEq)
 
@@ -64,12 +89,14 @@ def drawFunction(N,start,end, function):
 
 	ax.plot(xPoints, yPoints, color = "blue")
 
+eq = f + Derivative(f, x) + (Derivative(f, x, 2) + exp(x/2)) + 5*x**4
+secondOrderTaylorEq(-20, 1, 0, 5, eq)
+secondOrderTaylorEq(-10, 1, 0, 5, eq)
 
-firstOrderTaylorEq(-20,0,40)
-firstOrderTaylorEq(-10,0,40)
-firstOrderTaylorEq(0,0,40)
-firstOrderTaylorEq(10,0,40)
-firstOrderTaylorEq(20,0,40)
+secondOrderTaylorEq(0, 1, 0, 5, eq)
+
+secondOrderTaylorEq(10, 1, 0, 5, eq)
+secondOrderTaylorEq(20, 1, 0, 5, eq)
 
 
 plt.show()
